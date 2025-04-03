060abe07b31499a857346d219f641428b11f1d97
PhamTuan <phamtuan301104@gmail.com>
2025-04-03 09:43:49 +0700

    UI for shop

--- run.py
+++ run.py
@@ -8,5 +8,21 @@ app = Flask(__name__,
 def index():
     return render_template('index.html')
 
+@app.route('/products')
+def products():
+    return render_template('products.html')
+
+@app.route('/combos')
+def combos():
+    return render_template('combos.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/cart')
+def cart():
+    return render_template('cart.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
